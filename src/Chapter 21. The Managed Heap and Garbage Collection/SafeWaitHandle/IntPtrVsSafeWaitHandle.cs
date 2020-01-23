48f8c19195707f4a042c2026daa5449be720028a
Shukurdin <ShukurdinAi@halykban.kz>
2020-01-23 10:14:01 +0600

    Chapter 21. SafeWaitHandle

---
+++ src/Chapter 21. The Managed Heap and Garbage Collection/SafeWaitHandle/IntPtrVsSafeWaitHandle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace SafeWaitHandle
+{
+    internal static class IntPtrVsSafeWaitHandle
+    {
+        [DllImport("Kernel32", CharSet = CharSet.Unicode, EntryPoint="CreateEvent")]
+        private static extern IntPtr CreateEventAsIntPrt( // <- this prototype is not robust
+            IntPtr pSecurityAttribute,
+            bool manualReset,
+            bool initialSate,
+            string name);
+
+        [DllImport("Kernel32", CharSet = CharSet.Unicode, EntryPoint = "CreateEvent")]
+        private static extern Microsoft.Win32.SafeHandles.SafeWaitHandle CreateEventAsSafeWaitHandle( // <- is robust
+            IntPtr pSecurityAttribute,
+            bool manualReset,
+            bool initialSate,
+            string name);
+
+        public static (IntPtr ptr, Microsoft.Win32.SafeHandles.SafeWaitHandle safeWait) Create()
+        {
+            var handle = CreateEventAsIntPrt(IntPtr.Zero, false, false, null);
+            var swh = CreateEventAsSafeWaitHandle(IntPtr.Zero, false, false, null);
+
+            return (handle, swh);
+        }
+    }
+}
---
+++ src/Chapter 21. The Managed Heap and Garbage Collection/SafeWaitHandle/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+
+namespace SafeWaitHandle
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            while(true)
+            {
+                var thread = new Thread(_ =>
+                {
+                    // Memory leak with IntPtr when thread aborted
+                    var (ptr, swh) = IntPtrVsSafeWaitHandle.Create();
+
+                    Console.WriteLine("IntPtr = {0}, SafeWaitHandle = {1}", ptr, swh);
+                });
+
+                thread.Start();
+
+                Thread.Sleep(500);
+
+                thread.Abort();
+            }
+        }
+    }
+}
